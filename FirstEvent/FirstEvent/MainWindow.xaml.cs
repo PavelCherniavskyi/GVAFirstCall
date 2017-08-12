d8a7ac81593045c6106caf9779b0a53601d8a59c
Pavel Cherniavskyi <cherniavskyi@gmail.com>
2017-08-12 21:16:43 +0300

    UI almost done exept scrollviewer

--- FirstEvent/FirstEvent/MainWindow.xaml.cs
+++ FirstEvent/FirstEvent/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,13 +22,45 @@ namespace FirstEvent
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly DataGridView _dataGridView = new DataGridView();
         public MainWindow()
         {
             InitializeComponent();
 
-            
-
         }
-        
+
+        private void ContactsAddBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            DataContext = _dataGridView.AddContact("Mother", "Petya", "456", "he is good");
+            
+        }
+
+        private void ContactsCopyBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            var context = DataContext as ObservableCollection<Contact>;
+            if (context == null)
+                return;
+            if (DataGridContacts.SelectedIndex == context.Count - 1)
+            {
+                context.Add(context[DataGridContacts.SelectedIndex]);
+            }
+            else
+            {
+                context.Insert(DataGridContacts.SelectedIndex + 1, context[DataGridContacts.SelectedIndex]);
+            }
+            
+            DataContext = context;
+        }
+
+        private void ContactsDeleteBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            var context = DataContext as ObservableCollection<Contact>;
+            if (context == null)
+                return;
+            context.RemoveAt(DataGridContacts.SelectedIndex);
+            DataContext = context;
+        }
+
+       
     }
 }
